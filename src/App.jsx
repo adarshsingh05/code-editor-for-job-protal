fb0042191109a23203894bd39509513dcbb08746
adarsh singh <adarsh.22bce7232@vitapstudent.ac.in>
2024-09-24 17:49:52 +0530

    adding the header part

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 import CodeEditor from "./assets/components/CodeEditor.jsx";
+import logo from "../src/assets/images/logo.png";
 function App() {
   return (
-    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
-      <CodeEditor />
-    </Box>
+    <>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        h="100px"
+        fontSize="2xl"
+        textAlign="center"
+      >
+<Image src={logo} alt="logo" h="50px" w="85px" mr={4} /> {/* Adjust height and width */}
+AI-Powered Code editor by Hirrd!
+      </Box>
+      <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+        <CodeEditor />
+      </Box>
+    </>
   );
 }
 
